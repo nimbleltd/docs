e3a5a341187a3f6553ee05879cde2a00e3b074d2
Cole Krumbholz <cole.krumbholz@gmail.com>
2012-05-30 00:58:10 -0400

    updated run command not to overwrite build folder with local server settings

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-bfdocs --server --port=4444 --watch "${DIR}/../docs/manifest.json" "${DIR}/.."
+bfdocs --server --port=4444 --watch "${DIR}/../docs/manifest.json" "${DIR}/../server"
